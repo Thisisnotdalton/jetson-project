9da7b6b71790a2388e1f8ccdc338b5c873692d8f
Dalton Nickerson <dnickerson001@idtech.com>
2022-04-20 19:22:39 -0400

    Added project.py

---
+++ project.py
@@ -0,0 +1,38 @@
+
+
+
+# Problem identify which of my cats is in the room with ML/Computer Vision
+
+# Figure out which network/architecture we want to use
+# potentially detecting multiple cats at once? mobile detect focused networks instead of classificaiton
+
+# Pretrained model? 
+# training data -> pictures of my cats with boundings boxes labeled 
+# use the nvidia camera-collect tool to easily acquire/label data
+
+
+
+# Imagine we have trained our model
+# load the model
+# set up our video inputs/outputs
+# loop
+#     get latest video feed as an image
+#     process the image with our detection network
+#     if the cat is Purrnest:
+#         do X
+#     if the cat is Kiki:
+#         do Y
+#     if the cat is Haru:
+#         do Z
+
+
+
+#     if image contains delivery person and package:
+#         say thanks to delivery person
+#     if image contains package and random person:
+#         say get off my property1
+#     if :
+#         do Z
+
+
+
